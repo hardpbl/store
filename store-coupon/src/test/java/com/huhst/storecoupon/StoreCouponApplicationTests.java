a782c96c9459f80849d04f0aaf9d1caaa48c507e
20811 <bailiang.pan@hand-china.com>
2020-10-01 16:39:31 +0800

    [add] 项目初始化

---
+++ store-coupon/src/test/java/com/huhst/storecoupon/StoreCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huhst.storecoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ store-member/src/test/java/com/huhst/storemember/StoreMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huhst.storemember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
